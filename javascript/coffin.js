7aa2476ef48fc4a483127cd33973143d16b162b2
Jacob Thornton <jacobthornton@gmail.com>
2012-06-24 13:09:50 -0700

    rewrite js + css... works like 100x better :D

--- javascript/coffin.js
+++ javascript/coffin.js
@@ -11,109 +11,159 @@
    * coffin toggle for mobile
    */
 
-  $(function () {
+  // start of X scroll pos
+  var xStart;
 
-    var open       = 'coffin-open'
-    var $body      = $('body')
-    var $stage     = $('.stage')
-    var touchstart = 'ontouchstart' in document.documentElement ? 'touchstart' : 'click'
+  // start of Y scroll pos
+  var yStart;
 
-    $body
-      .delegate('.coffin-tab', 'click', function (e) { e.preventDefault() })
-      .delegate('[data-coffin="click"]', 'click'   , toggleCoffin)
-      .delegate('[data-coffin="touch"]', touchstart, toggleCoffin)
+  // direction of scroll
+  var direction;
 
-    function translate3d (open) {
-      return 'translate3d(' + (open  ? '210' : -1 * Math.max(210 - window.scrollX, 210)) + 'px,0,0)'
+  // movement offset
+  var xMovement = 0;
+
+  // end scroll position
+  var xEnd = 0;
+
+  // percent ofscroll offset to trigger close/open action
+  var fraction = 1/5;
+
+  // is coffin open
+  var isOpen = false;
+
+  // page element
+  var page = document.querySelector('.page');
+
+  // is this a touch device
+  var isTouch = 'ontouchstart' in document.documentElement;
+
+  // declaritive selectors
+  var clickSelector = '[data-coffin=click]';
+  var touchSelector = '[data-coffin=touch]';
+
+  // window size cache
+  var windowSize = document.body.offsetWidth;
+
+  // if isTouch, map declaritive touch listeners to click events
+  if (isTouch) clickSelector += ', ' + touchSelector;
+
+  // function for 3d transforms
+  function translate3d (i) {
+      page.style.webkitTransform = 'translate3d(' + i + 'px,0,0)';
+  }
+
+  // simple helper to force coffin close
+  function closeCoffin () {
+      xMovement = 0;
+      touchEnd();
+  }
+
+  // detect the closest element based on a selctor. For simple delegation.
+  function closest (element, selector) {
+    while (element && !element.webkitMatchesSelector(selector)) {
+        element = element.parentNode;
+        if (!element.webkitMatchesSelector) return false;
     }
+    return element;
+  }
 
-    function toggleCoffin() {
+  // handle touch end
+  function touchEnd () {
 
-      var isOpen = $body.hasClass(open)
+      // if window isn't mobile, than exit
+      if (windowSize > 767) return;
 
-      function transitionComplete () {
+      // remove transition and handler on transitionEnd
+      var transitionEnd = function () {
+          page.style.webkitTransition = '';
+          page.removeEventListener('webkitTransitionEnd', transitionEnd);
+      };
 
-        if (isOpen) $body.removeClass(open)
+      // calculate which side to transition to
+      xEnd = xMovement <= (isOpen ? (270 - (fraction * 270)) : fraction * 270) ? 0 : 270;
 
-        $stage.css({
-          '-webkit-transform'   : '',
-          '-webkit-transition'  : '',
-          'left': !isOpen ? 210 : ''
-        })
+      // check if transitioned open
+      isOpen = xEnd === 270;
 
-        $stage.unbind('webkitTransitionEnd.coffin')
+      // set transition property for animation
+      page.style.webkitTransition = '-webkit-transform .1s linear';
 
-        if (isOpen) {
-          $body
-            .unbind('touchstart.coffin')
-            .unbind('touchmove.coffin')
-            .unbind('touchend.coffin')
-        }
+      // tranform element along x axis
+      translate3d(xEnd);
 
+      // listen for transition complete
+      page.addEventListener('webkitTransitionEnd', transitionEnd);
+
+  }
+
+  // handle resize event
+  window.addEventListener('resize', function (e) {
+
+      // if window is resized greater than mobile, then remove any transforms
+      if ((windowSize = document.body.offsetWidth) > 767) page.style.webkitTransform = '';
+
+  });
+
+
+  // handle touch start event
+  window.addEventListener('touchstart', function (e) {
+
+      // if window isn't mobile, than exit
+      if (windowSize > 767) return;
+
+      // reset direction property
+      direction = '';
+
+      // reset xMovement to left/right position
+      xMovement = isOpen ? 270 : 0;
+
+      // set touch start position for x axis
+      xStart = e.touches[0].screenX;
+
+      // set touch start position for y axis
+      yStart = e.touches[0].screenY;
+
+  });
+
+
+  // handle touchmove event
+  window.addEventListener('touchmove', function (e) {
+
+      // if window isn't mobile, than exit
+      if (windowSize > 767 || direction == 'vertical') return;
+
+      var xOffset = Math.abs(e.touches[0].screenX - xStart);
+      var yOffset = Math.abs(e.touches[0].screenY - yStart);
+
+      if (yOffset > xOffset) return direction = 'vertical';
+
+      direction = 'horizontal';
+
+      // calcuate movement based on last scroll pos
+      xMovement = e.touches[0].screenX - xStart + xEnd;
+
+      // if xmovement is within valid range, scroll page
+      if (xMovement <= 270 && xMovement >= 0) {
+          translate3d(xMovement);
       }
 
-      if (!isOpen) $body.addClass(open)
+  });
 
-      $stage.bind('webkitTransitionEnd.coffin', transitionComplete)
+  // listen for touchend event
+  window.addEventListener('touchend', touchEnd);
 
-      $stage.css({
-        '-webkit-transform' : translate3d(!isOpen),
-        '-webkit-transition': '-webkit-transform .1s linear'
-      })
+  // listen to click events on declartive markup
+  window.addEventListener('click', function (e) {
+      closest(e.target, clickSelector) && closeCoffin();
+  });
 
-      if (isOpen || touchstart == 'click') return
+  // if not touch exit early
+  if (!isTouch) return;
 
-      setTimeout(function () {
+  // listen to touch start events on declartive markup
+  window.addEventListener('touchstart', function (e) {
+      closest(e.target, touchSelector) && closeCoffin();
+  });
 
-        var xStart
-        var yStart
-
-        $body
-
-          .bind('touchstart.coffin', function (e) {
-            xStart = e.touches[0].screenX
-            yStart = e.touches[0].screenY
-          })
-
-          .bind('touchmove.coffin', function (e) {
-            var xMovement = Math.abs(e.touches[0].screenX - xStart)
-            var yMovement = Math.abs(e.touches[0].screenY - yStart)
-            if ((yMovement * 10) > xMovement) {
-              e.preventDefault()
-            }
-          })
-
-          .bind('touchend.coffin', function (e) {
-
-            if (!window.scrollX) return
-
-            var scrollX    = window.scrollX
-            var willScroll = (210 - scrollX) >= 0
-            var interval   = setInterval(function () {
-
-              if (scrollX != window.scrollX) return scrollX = window.scrollX
-
-              clearInterval(interval)
-
-              isOpen = true
-
-              if (willScroll) $stage.one('webkitTransitionEnd', transitionComplete)
-
-              $stage.css({
-                '-webkit-transform' : translate3d(),
-                '-webkit-transition': '-webkit-transform .1s linear'
-              })
-
-              if (!willScroll) transitionComplete()
-
-            }, 10)
-
-          })
-
-      }, 0)
-
-    }
-
-  })
-
-}(window.Zepto || window.jQuery)
+}();
