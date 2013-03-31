89f416097bbc4327b6aff96becb7b90df0da5426
Jacob Thornton <jacobthornton@gmail.com>
2013-03-30 19:02:31 -0700

    a bunch of updates to style and tightening of code and improvements to thingz

--- javascript/coffin.js
+++ javascript/coffin.js
@@ -7,6 +7,9 @@
 
 !function () {
 
+    // touchend listener
+    var touchEnd;
+
     // start of X scroll pos
     var xStart;
 
@@ -23,7 +26,10 @@
     var xEnd = 0;
 
     // percent ofscroll offset to trigger close/open action
-    var fraction = 1/5;
+    var fraction = 3/10;
+
+    // window size cache
+    var windowSize = window.innerWidth;
 
     // is coffin open
     var isOpen = false;
@@ -34,96 +40,86 @@
     // coffin element
     var coffin = document.querySelector('.coffin');
 
+    // test element
+    var skeletor = document.createElement('skeletor')
+
     // is this a touch device
-    var isTouch = 'ontouchstart' in document.documentElement;
+    var isTouch = 'ontouchstart' in skeletor;
 
-    // declaritive selectors
-    var clickSelector = '[data-coffin=click]';
-    var touchSelector = '[data-coffin=touch]';
+    // transition end event names
+    var transitionEndEventNames = {
+       'WebkitTransition' : ['webkitTransitionEnd', 'webkitTransform', '-webkit-transform'],
+       'MozTransition'    : ['transitionend', 'transform', 'transform'],
+       'transition'       : ['transitionend', 'transform', 'transform']
+    }
 
-    // window size cache
-    var windowSize = document.body.offsetWidth;
+    // the transitionend event name
+    var transitionEndEventName;
 
-    // if isTouch, map declaritive touch listeners to click events
-    if (isTouch) clickSelector += ', ' + touchSelector;
+    // the transition property
+    var transitionProperty;
+
+    // transform property in js
+    var transformProperty;
+
+    // transform property in css
+    var transformCSSProperty;
+
+    // define the property and event name
+    for (name in transitionEndEventNames) {
+        if (skeletor.style[name] !== undefined) {
+            transitionProperty = name
+            transitionEndEventName = transitionEndEventNames[name][0]
+            transformProperty = transitionEndEventNames[name][1]
+            transformCSSProperty = transitionEndEventNames[name][2]
+            break
+        }
+    }
 
     // function for 3d transforms
     function translate3d (i) {
-        page.style.webkitTransform = 'translate3d(' + i + 'px,0,0)';
+        page.style[transformProperty] = 'translate3d(' + i + 'px,0,0)';
+    }
+
+    // clearTransform
+    function clearTransform () {
+        page.style[transformProperty] = '';
+    }
+
+    // simple helper to toggle coffin
+    function toggleCoffin () {
+        direction = 'horizontal'
+        isOpen ? closeCoffin() : openCoffin()
+    }   
+
+    // simple helper to force coffin open
+    function openCoffin () {
+        xMovement = Infinity;
+        touchEnd();
     }
 
     // simple helper to force coffin close
     function closeCoffin () {
-        if (windowSize > 767) return;
-        xMovement = 0;
+        xMovement = -Infinity;
         touchEnd();
     }
 
-    // detect the closest element based on a selctor. For simple delegation.
+    // try a native matchesSelector on a selector with an element
+    function matchesSelector(element, selector) {
+        var matcher = element.matchesSelector || element.mozMatchesSelector || element.webkitMatchesSelector || element.oMatchesSelector
+        return matcher && matcher.call(element, selector);
+    }
+
+    // detect the closest element based on a selector. For simple delegation.
     function closest (element, selector) {
-      while (element && !element.webkitMatchesSelector(selector)) {
+      while (element && !matchesSelector(element, selector)) {
           element = element.parentNode;
-          if (!element.webkitMatchesSelector) return;
       }
       return element;
     }
 
-    // handle touch end
-    function touchEnd () {
-
-        //debugger
-
-        // if window isn't mobile, than exit
-        if (windowSize > 767 || direction == 'vertical') return;
-
-        var transitionEnd = function () {
-
-            // remove transition and handler on transitionEnd
-            if (!isOpen) page.style.webkitTransform = '';
-            page.style.webkitTransition = '';
-            page.removeEventListener('webkitTransitionEnd', transitionEnd);
-
-            // remove the pulling
-            coffin.classList.remove('coffin-pulling');
-
-            // toggle the coffin open class
-            coffin.classList[isOpen ? 'add' : 'remove']('coffin-open');
-        };
-
-        // calculate which side to transition to
-        xEnd = xMovement <= (isOpen ? (270 - (fraction * 270)) : fraction * 270) ? 0 : 270;
-
-        // check if transitioned open
-        isOpen = xEnd === 270;
-
-        // set transition property for animation
-        page.style.webkitTransition = '-webkit-transform .1s linear';
-
-        // tranform element along x axis
-        translate3d(xEnd);
-
-        // listen for transition complete
-        page.addEventListener('webkitTransitionEnd', transitionEnd);
-
-        // if exit is at edge, force transitionEnd because transition won't be fired anyways
-        if (xMovement == 270) transitionEnd();
-
-    }
-
-    // handle resize event
-    window.addEventListener('resize', function (e) {
-
-        // if window is resized greater than mobile, then remove any transforms
-        if ((windowSize = document.body.offsetWidth) > 767) page.style.webkitTransform = '';
-
-    });
-
-
     // handle touch start event
     window.addEventListener('touchstart', function (e) {
-
-        // if window isn't mobile, than exit
-        if (windowSize > 767) return;
 
         // reset direction property
         direction = '';
@@ -139,15 +135,14 @@
 
     });
 
-
     // handle touchmove event
     window.addEventListener('touchmove', function (e) {
 
         // don't allow scrolling the page up and down when nav open
         if (direction == 'vertical' && isOpen) e.preventDefault();
 
-        // if window isn't mobile, than exit
-        if (windowSize > 767 || direction == 'vertical') return;
+        // if direction is vertical then exit
+        if (direction == 'vertical') return;
 
         // calculate offsets to see if scroll direciton is vertical or horizontal
         var xOffset = Math.abs(e.touches[0].screenX - xStart);
@@ -176,19 +171,51 @@
     });
 
     // listen for touchend event
-    window.addEventListener('touchend', touchEnd);
+    window.addEventListener('touchend', (touchEnd = function (e) {
 
-    // listen to click events on declartive markup
+        // if direction isn't horizontal than exit (w maybe toggle)
+        if (direction != 'horizontal') return;
+
+        var transitionEnd = function () {
+
+            // reset transform
+            if (!isOpen) clearTransform();
+
+            page.style[transitionProperty] = '';
+
+            page.removeEventListener(transitionEndEventName, transitionEnd);
+
+            // remove the pulling
+            coffin.classList.remove('coffin-pulling');
+
+            // toggle the coffin open class
+            coffin.classList[isOpen ? 'add' : 'remove']('coffin-open');
+
+        };
+
+        // calculate which side to transition to
+        xEnd = xMovement <= (isOpen ? (270 - (fraction * 270)) : fraction * 270) ? 0 : 270;
+
+        // check if transitioned open
+        isOpen = xEnd === 270;
+
+        // set transition property for animation
+        page.style[transitionProperty] = transformCSSProperty + ' .1s linear';
+
+        // tranform element along x axis
+        translate3d(xEnd);
+
+        // listen for transition complete
+        page.addEventListener(transitionEndEventName, transitionEnd);
+
+        // if exit is at edge, force transitionEnd because transition won't be fired anyways
+        if (xMovement == 270) transitionEnd();
+
+    }));
+
     window.addEventListener('click', function (e) {
-        closest(e.target, clickSelector) && closeCoffin();
-    });
-
-    // if not touch exit early
-    if (!isTouch) return;
-
-    // listen to touch start events on declartive markup
-    window.addEventListener('touchstart', function (e) {
-        closest(e.target, touchSelector) && closeCoffin();
-    });
-
+        if (!direction && closest(e.target, '[data-coffin=toggle]')) toggleCoffin()
+        return direction = '';  
+    })
+            
 }();
